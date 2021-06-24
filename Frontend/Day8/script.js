ebc6e47545183137c59db07ff8a458e94932e89a
Amber Prakash <amberprakashverma21@gmail.com>
2021-06-24 20:49:38 +0530

    CSS100days completed

---
+++ Frontend/Day8/script.js
@@ -0,0 +1,15 @@
+const center=document.querySelector(".center");
+
+for(let i=0;i<400;i++){
+    const item=document.createElement("div");
+    item.addEventListener('click',()=>{
+        const check=item.style.backgroundColor;
+        if(check=="white"){
+            item.style.backgroundColor="black";
+        }
+        else{
+            item.style.backgroundColor="white";
+        }
+    });
+    center.append(item);
+}
