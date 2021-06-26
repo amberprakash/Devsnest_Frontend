9c5e7563ea491947c5dd0a46d25c6953fbe19ec2
Amber Prakash <amberprakashverma21@gmail.com>
2021-06-26 20:15:02 +0530

    bugs fixed

--- Frontend/Day8/script.js
+++ Frontend/Day8/script.js
@@ -2,14 +2,15 @@ const center=document.querySelector(".center");
 
 for(let i=0;i<400;i++){
     const item=document.createElement("div");
+    center.append(item);
     item.addEventListener('click',()=>{
         const check=item.style.backgroundColor;
-        if(check=="white"){
+        if(check==="white"){
             item.style.backgroundColor="black";
         }
         else{
             item.style.backgroundColor="white";
         }
-    });
-    center.append(item);
+    })
+    
 }
