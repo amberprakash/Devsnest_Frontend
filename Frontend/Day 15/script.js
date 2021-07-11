b6a641cc613d119057afb5583c874fdbb9268d2e
Amber Prakash <amberprakashverma21@gmail.com>
2021-07-11 20:34:40 +0530

    Day 15 Completed

--- Frontend/Day 15/script.js
+++ Frontend/Day 15/script.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello World!!!</h1>,
+    React.createElement('h1',{},'Hello World!!!'),
     document.querySelector('#myworld')
 )
