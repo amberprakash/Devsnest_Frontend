2b28d42a8f5927b9c505f96548a8f388941db0cd
Amber Prakash <amberprakashverma21@gmail.com>
2021-07-13 00:31:11 +0530

    Tha 16.1 Completed

---
+++ Frontend/day-16/src/App.jsx
@@ -0,0 +1,10 @@
+import Card from './components/Card.jsx';
+import "./style.css";
+function App(){
+    return(
+    <div className="App">
+        <Card />
+    </div>
+    );
+}
+export default App;
---
+++ Frontend/day-16/src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return(
+        <div className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ty9vGV4Dd0ER1GBN739zjkeYxnLVGs1-tA&usqp=CAU" />
+            <h1 className="heading">React card</h1>
+             <p className="heading">This is a Card</p>
+
+        </div>
+    )
+}
+export default Card;
