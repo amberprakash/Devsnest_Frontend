94d510023ef218072c384db1a5815b96c3b177b9
Amber Prakash <amberprakashverma21@gmail.com>
2021-07-14 13:09:44 +0530

    Day 16 Completed

---
+++ Frontend/Day 16.2/src/components/Board.jsx
@@ -0,0 +1,11 @@
+// import Box from "./Box__w";
+import InnerBoard from "./InnerBoard";
+
+function Board(){
+    return(
+        <div className="board">
+            <InnerBoard />
+        </div>
+    )
+}
+export default Board;
