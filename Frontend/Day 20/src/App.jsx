331e81aadf2ad6fcc9d4e027e3a46718ff7d04e1
Amber Prakash <amberprakashverma21@gmail.com>
2021-07-22 20:23:59 +0530

    Day 20 Completed

---
+++ Frontend/Day 20/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="main">
+      <h1 className="heading">Calorie Read Only</h1>
+      <Card />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Frontend/Day 20/src/components/Card.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+const Card = () => {
+  const [items, setItems] = useState([
+    {
+      name: "Pizza",
+      cal: 56,
+    },
+    {
+      name: "Burger",
+      cal: 69,
+    },
+    {
+      name: "Coke",
+      cal: 500,
+    },
+    {
+      name: "Brownie",
+      cal: 180,
+    },
+    {
+      name: "Fried Rice",
+      cal: 90,
+    },
+    {
+      name: "Lassania",
+      cal: 200,
+    },
+    {
+      name: "Pani Puri",
+      cal: 0,
+    },
+  ]);
+
+  const del = (i) => {
+    const newItems = items.filter((e, index) => index !== i);
+    setItems(newItems);
+  };
+
+//   useEffect(() => {
+//     if (items.length === 0) {
+//       alert("Its EMpty . ANsh");
+//     }
+//   }, [items]);
+
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          {items.map((ele, index) => {
+            return (
+              <div key={index} className="content">
+                <div className="names">
+                  <h1>{ele.name}</h1>
+                  <button onClick={() => del(index)}>Delete</button>
+                </div>
+                <h2>you have consumed {ele.cal} cals today</h2>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
